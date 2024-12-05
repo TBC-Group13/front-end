e70c458280ea84b80b9093e6b68c01bfee688ec9
Elene <ellebuto@gmail.com>
2024-12-05 16:25:12 +0400

    fix conflict

--- src/pages/createQuestion/index.tsx
+++ src/pages/createQuestion/index.tsx
@@ -1,27 +1,12 @@
 import { Input } from '@/components/ui/input';
 import { QuestionLabel } from './QuestionLabel';
 import { Textarea } from '@/components/ui/textarea';
-<<<<<<< Updated upstream
-import { Label } from '@radix-ui/react-label';
-import {
-  Command,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command';
-// import { useState } from 'react';
-
-export const CreateQuestion = () => {
-  // const [inputValue, setInputValue] = useState('');
-  const tags = ['React', 'JS'];
-=======
 import { AddTab } from './AddTab';
 import { Button } from '@/components/ui/button';
 
 export const CreateQuestion = () => {
->>>>>>> Stashed changes
   return (
-    <div className="flex flex-col gap-8 lg:mx-80 lg:mt-10">
+    <div className="flex flex-col lg:mx-80 lg:mt-10 lg:gap-7">
       <div>
         <QuestionLabel text="Question Title" />
         <Input />
@@ -30,12 +15,11 @@ export const CreateQuestion = () => {
         <QuestionLabel text="Question Description" />
         <Textarea className="lg:h-40" />
       </div>
-
       <div>
         <QuestionLabel text="Tabs" />
         <AddTab />
       </div>
-      <Button>Create</Button>
+      <Button className="w-[100%]">Create</Button>
     </div>
   );
 };
